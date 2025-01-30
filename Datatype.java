4121a9bdb7405908c1359ebcca9fa4769215f5cf
venkatsai963 <venkatgunji298@gmail.com>
2025-01-30 22:30:05 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,17 @@
+public class Datatype {
+    public static void main(String[] args) {
+        int a=12;
+        float b =12.3f;
+        double c=2.443;
+        String d="hello";
+        char e='h';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+     
+
+
+    }
+}
